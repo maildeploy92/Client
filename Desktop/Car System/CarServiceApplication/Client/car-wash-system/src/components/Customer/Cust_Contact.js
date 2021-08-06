a012d68796911187e1800bcddf146bf2beca2d10
maildeploy92 <maildeploy@gmail.com>
2021-08-06 13:23:20 +0800

    a

---
+++ Desktop/Car System/CarServiceApplication/Client/car-wash-system/src/components/Customer/Cust_Contact.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Cust_Contact() {
+  return (
+    <div>
+      <h1>Contact Us</h1>
+      <h1>Contact Us</h1>
+      <h1>Contact Us</h1>
+    </div>
+  );
+}
+
+export default Cust_Contact;
